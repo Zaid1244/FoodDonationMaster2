c4d12f197d39b46a70ed6a6b51ec00d854b3e5df
MOHD ZAID ABBAS <125466269+Zaid1244@users.noreply.github.com>
2025-11-27 19:24:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -587,8 +587,9 @@ app.get("/logout", (req, res) => {
   res.render("intro");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
